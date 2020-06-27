6e78984832716855adfce51e0e46b4a2e43de5ea
murselsenay <murselsenay@gmail.com>
2020-06-27 18:08:29 +0300

    Initial commit

---
+++ Assets/Scripts/CoffeeHill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeHill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "GrindedCoffee")
+        {
+            transform.localScale += new Vector3(0, 0, 0.01f);
+            Debug.Log("d");
+            Destroy(other.gameObject);
+        }
+    }
+  
+}
---
+++ Assets/Scripts/CoffeeSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeSpawner : MonoBehaviour
+{
+    public GameObject grindedCoffee;
+    public int coffeeAmount = 5;
+    void Start()
+    {
+        StartCoroutine(grindCoffee());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public IEnumerator grindCoffee()
+    {
+        while (coffeeAmount>0)
+        {
+            Instantiate(grindedCoffee, transform.position, Quaternion.identity);
+            coffeeAmount -= 1;
+            yield return new WaitForSeconds(2f);
+        }
+       
+    }
+}
